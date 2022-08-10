f10ce8ceeadeb61eaf499e5594a26cc49ff0cfc6
hukman@codedevid.onmicrosoft.com <hukman@code.id>
2022-08-10 14:56:03 +0700

    -add permission internet -refactor fragment_first.xml to fragment_splash.xml -refactor FirstFragment.kt to SplashFragment.kt -refactor UI activity_main.xml and MainActivity.kt

---
+++ app/src/main/java/com/example/movieApp/fragment/SplashFragment.kt
@@ -0,0 +1,103 @@
+package com.example.movieApp.fragment
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import com.example.movieApp.R
+import com.example.movieApp.databinding.FragmentSplashBinding
+import com.example.movieApp.model.getGenre.GetGenreResponse
+import com.example.movieApp.service.RetrofitBuilder
+import com.example.movieApp.util.serverError
+import com.example.movieApp.util.showSnackBarLong
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
+import retrofit2.HttpException
+import java.util.concurrent.TimeoutException
+
+/**
+ * A simple [Fragment] subclass as the default destination in the navigation.
+ */
+class SplashFragment : Fragment() {
+
+    private var movieAPI = RetrofitBuilder.movieAPI
+
+    private var _binding: FragmentSplashBinding? = null
+    private var _genreResponse: GetGenreResponse? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        _binding = FragmentSplashBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        getGenreData()
+        Log.d("GENRE_RESPONSE", _genreResponse.toString())
+        if (_genreResponse != null){
+            binding.root.findNavController().navigate(R.id.action_SplashFragment_to_SecondFragment)
+        }
+    }
+
+
+
+    private fun getGenreData() = runBlocking {
+        try {
+            binding.progressBar.visibility = View.VISIBLE
+            val result = withContext(Dispatchers.IO) { movieAPI.getGenreAsync() }
+
+            _genreResponse = if (result.genres != null) {
+                result
+            } else {
+                GetGenreResponse(genres = null)
+            }
+        } catch (e: HttpException) {
+            if (e.code() != 500) {
+                val body = e.response()!!.errorBody()
+                val errorConverter = RetrofitBuilder.errorConverter
+                val errorParser = withContext(Dispatchers.IO) {
+                    errorConverter.convert(body!!)
+                }
+                withContext(Dispatchers.Main) {
+                    showSnackBarLong(errorParser?.statusMessage ?: e.message())
+                    Log.e(
+                        activity?.javaClass?.simpleName,
+                        errorParser?.statusMessage ?: e.message()
+                    )
+
+                }
+            } else {
+                serverError(e)
+            }
+        } catch (e: TimeoutException) {
+            withContext(Dispatchers.Main) {
+                showSnackBarLong("Request Timeout")
+            }
+        } catch (e: Exception) {
+            withContext(Dispatchers.Main) {
+                showSnackBarLong("Unknown Error")
+                e.printStackTrace()
+            }
+        } finally {
+            binding.progressBar.visibility = View.GONE
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
