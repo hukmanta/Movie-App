e6fa2cf1155f9550f7c656d0367f1a0187b9a3a8
hukman@codedevid.onmicrosoft.com <hukman@code.id>
2022-08-10 14:52:21 +0700

    -create service

---
+++ app/src/main/java/com/example/movieApp/service/MovieAPI.kt
@@ -0,0 +1,45 @@
+package com.example.movieApp.service
+
+import com.example.movieApp.BuildConfig
+import com.example.movieApp.model.getGenre.GetGenreResponse
+import com.example.movieApp.model.getMovieByGenre.GetMovieByGenreResponse
+import com.example.movieApp.model.getMovieDetail.GetMovieDetailResponse
+import com.example.movieApp.model.getReview.GetReviewResponse
+import retrofit2.http.*
+import java.net.ConnectException
+
+interface MovieAPI {
+
+    /***
+     * @return [GetGenreResponse]
+     * **/
+    @GET("genre/movie/list")
+    @Throws(ConnectException::class)
+    suspend fun getGenreAsync(@Header("Authorization") accessToken: String = "Bearer ${BuildConfig.API_KEY}", @Query("language") language: String="id"): GetGenreResponse
+
+    /***
+    * @return [GetMovieByGenreResponse]
+     *@param parameters => hashMapOf("language" to "id", "with_genres" to 18, "page" to 1)
+    * **/
+    @GET("/discover/movie")
+    @Throws(ConnectException::class)
+    suspend fun getMovieByGenreAsync(@Header("Authorization") accessToken: String = "Bearer ${BuildConfig.API_KEY}", @QueryMap(encoded = true) parameters: Map<String, Any>): GetMovieByGenreResponse
+
+    /***
+     * @return [GetReviewResponse]
+     *@param parameters => hashMapOf("language" to "id","page" to 1)
+     * @param id => movie_ID : Int
+     * **/
+    @GET("/movie/{id}/reviews")
+    @Throws(ConnectException::class)
+    suspend fun getUserReviewAsync(@Header("Authorization") accessToken: String = "Bearer ${BuildConfig.API_KEY}", @Path(value = "id", encoded = true) id: Int, @QueryMap(encoded = true) parameters: Map<String, Any>): GetReviewResponse
+
+
+    /***
+     * @return [GetMovieDetailResponse]
+     * @param id => movie_ID : Int
+     * **/
+    @GET("genre/movie/{id}")
+    @Throws(ConnectException::class)
+    suspend fun getMovieDetailAsync(@Header("Authorization") accessToken: String = "Bearer ${BuildConfig.API_KEY}", @Path(value = "id", encoded = true)id: Int, @Query("language") language: String="id"): GetMovieDetailResponse
+}
---
+++ app/src/main/java/com/example/movieApp/service/RetrofitBuilder.kt
@@ -0,0 +1,43 @@
+package com.example.movieApp.service
+
+import com.example.movieApp.BuildConfig
+import com.example.movieApp.model.error.ErrorResponse
+import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
+import okhttp3.OkHttpClient
+import okhttp3.ResponseBody
+import retrofit2.Converter
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
+object RetrofitBuilder {
+
+    private const val BASE_URL = BuildConfig.SERVER_URL
+
+    private val clientBuilder = OkHttpClient.Builder().also {
+        it.connectTimeout(60, TimeUnit.SECONDS)
+        it.writeTimeout(60, TimeUnit.SECONDS)
+        it.readTimeout(60, TimeUnit.SECONDS)
+    }
+
+    private val retrofitBuilder :Retrofit.Builder by lazy{
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(clientBuilder.build())
+            .addCallAdapterFactory(CoroutineCallAdapterFactory())
+            .addConverterFactory(GsonConverterFactory.create())
+    }
+
+    val movieAPI : MovieAPI by lazy {
+        retrofitBuilder
+            .build()
+            .create(MovieAPI::class.java)
+    }
+
+    val errorConverter: Converter<ResponseBody, ErrorResponse> by lazy {
+        retrofitBuilder.build().responseBodyConverter<ErrorResponse>(
+            ErrorResponse::class.java, arrayOfNulls<Annotation>(0)
+        )
+    }
+
+}
